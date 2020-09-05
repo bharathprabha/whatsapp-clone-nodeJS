622127825ed60158ca3efa4560578efa74b22a7d
Bharath Prabha <bharathprabhakaran28@gmail.com>
2020-09-05 20:46:03 +0530

    completed chat functionality.

--- dbModel.js
+++ dbModel.js
@@ -4,6 +4,7 @@ const messageSchema = mongoose.Schema({
   message: String,
   user: String,
   time: String,
+  recevied: Boolean,
 });
 
-module.exports = mongoose.model("Group", messageSchema);
+module.exports = mongoose.model("msgcollections", messageSchema);
--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Group = require("./dbModel.js");
+const msgcollections = require("./dbModel.js");
+const Pusher = require("pusher");
+const cors = require("cors");
 
 // app config
 const app = express();
+const pusher = new Pusher({
+  appId: "1067293",
+  key: "96dbe6f0a211a4df079a",
+  secret: "f7c19c5c3a4ff59a6da4",
+  cluster: "ap2",
+  encrypted: true,
+});
 
 // app midddleware
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  next();
-});
+app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "*");
+//   next();
+// });
 
 //db config
 const user = "admin";
@@ -24,9 +35,29 @@ mongoose.connect(connectURL, {
   useUnifiedTopology: true,
 });
 
+const db = mongoose.connection;
+db.once("open", () => {
+  const msgCollection = db.collection("msgcollections");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    if (change.operationType == "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        user: messageDetails.user,
+        message: messageDetails.message,
+        time: messageDetails.time,
+        recevied: messageDetails.recevied,
+      });
+    } else {
+      console.log("Error triggering pusher");
+    }
+  });
+});
+
 // api endpoints
-app.get("/", (req, res) => {
-  Group.find((err, document) => {
+app.get("/message/sync", (req, res) => {
+  msgcollections.find((err, document) => {
     if (err) {
       res.status(500).send(err);
     } else {
@@ -35,10 +66,10 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/", (req, res) => {
+app.post("/message/post", (req, res) => {
   const dbMessage = req.body;
 
-  Group.create(dbMessage, (err, data) => {
+  msgcollections.create(dbMessage, (err, data) => {
     if (err) {
       res.status(500).send(err);
     } else {
